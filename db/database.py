26ace8dfc3894a422930af42b47c19e8bf4ac610
RAPP <raparrapp@gmail.com>
2026-05-29 10:27:08 -0500

    Agregar soporte SSL para Railway

--- db/database.py
+++ db/database.py
@@ -10,6 +10,7 @@ engine = create_async_engine(
     pool_pre_ping=True,
     pool_size=10,
     max_overflow=20,
+    connect_args={"ssl": "require"} if "railway" in (settings.database_url or "") else {},
 )
 
 AsyncSessionLocal = async_sessionmaker(
